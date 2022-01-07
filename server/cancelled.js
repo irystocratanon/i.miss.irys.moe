19e1390b553de36383f0d45341b51e55100503f9
anon <anon@i.miss.irys.moe>
2022-01-07 05:08:43 +0000

    update cancelled streams list

--- server/cancelled.js
+++ server/cancelled.js
@@ -1,5 +1,6 @@
 module.exports = {
     CancelledStreams: [
         "LZnGyMFJe0I", /* 【#みっころね24】新春!!爆笑!!みっころね24時間ぶっ通し生放送2022🎍【後半】 (short appearence in collab) */
+        "U_IW6CyYn1k" /* 【#ホロお正月CUP2022】ホロライブ、今年も熱くなれ!運も実力も手に入れろ【全体視点有/常闇トワ】(main POV for HOLO NEW YEARS CUP) */
     ]
 }
