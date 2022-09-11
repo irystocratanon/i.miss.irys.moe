b23ec168019062ac43ae4f1705eec7e2d66e8028
anon <anon@i.miss.irys.moe>
2022-09-11 21:29:01 +0000

    supas: add last-modified date for caching

--- pages/supas/[id].js
+++ pages/supas/[id].js
@@ -8,7 +8,12 @@ Home.getInitialProps = async function ({ req, res, query }) {
     if (res) {
         try {
             const supaReq = await fetch(`${process.env.SUPAS_ENDPOINT}/${query.id}`)
-            res.writeHead(supaReq.status, { "Content-Type": "text/html" });
+            let last_modified = supaReq.headers.get('Last-Modified')
+            last_modified = (last_modified) ? last_modified : (new Date(Date.now()).toUTCString())
+            res.writeHead(supaReq.status, {
+                "Content-Type": "text/html",
+                "Last-Modified": last_modified
+            });
             res.end(await supaReq.text());
         } catch (err) {
             console.error(err)
