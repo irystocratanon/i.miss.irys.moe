d43fbae6d79e4377576932bafefde74d249c3bb2
anon <anon@i.miss.irys.moe>
2022-11-11 13:55:11 +0000

    Update tailwind CSS (because the HTML is generated dynamically the CLI didn't pick up on some stuff, so I had to run it on a larger sample-size of HTML files)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./public/template-supas.html"],
+    content: ["./public/templates/*.html"],
   theme: {
     extend: {},
   },
