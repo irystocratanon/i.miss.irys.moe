7c29b1b02ac9d1ff3f5d364882626023c3434026
anon <anon@i.miss.irys.moe>
2022-11-12 09:59:07 +0000

    supas: leftpad SuperChat count

--- pages/index.js
+++ pages/index.js
@@ -404,7 +404,7 @@ export default function Home(props) {
     }
 
     const supas_js_pre_version = '0.1'
-    const supas_js_version = '1.3'
+    const supas_js_version = '1.4'
     const supas_css_version = '0.2'
 
 
