42dcaafeac05e951cb9b27d351e0045171b5de42
anon <anon@i.miss.irys.moe>
2022-11-20 13:56:53 +0000

    supas: update CSS

--- pages/index.js
+++ pages/index.js
@@ -405,7 +405,7 @@ export default function Home(props) {
 
     const supas_js_pre_version = '0.2'
     const supas_js_version = '1.6'
-    const supas_css_version = '0.5'
+    const supas_css_version = '0.6'
 
 
     return <div className={styles.site}>
