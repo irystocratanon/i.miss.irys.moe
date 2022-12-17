5b5e5f30c7d22438c3f25a2c6fba42a26e63cab3
anon <anon@i.miss.irys.moe>
2022-12-17 14:02:40 +0000

    supas: use an angle bracket instead of a legacy computing symbol (Chrome on Android lacks font support)

--- pages/index.js
+++ pages/index.js
@@ -405,7 +405,7 @@ export default function Home(props) {
 
     const supas_js_pre_version = '0.5'
     const supas_js_version = '2.1'
-    const supas_css_version = '0.9'
+    const supas_css_version = '1'
 
 
     return <div className={styles.site}>
