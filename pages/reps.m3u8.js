8fd88954d8920f7cd7a1c70cf7d86d9cf57ba671
anon <anon@i.miss.irys.moe>
2023-01-09 15:30:22 +0000

    reps: /reps.m3u8

---
+++ pages/reps.m3u8.js
@@ -0,0 +1,27 @@
+import performance from '../server/lib/get-performance.js'
+import getReps from "../server/reps_poller"
+export default function Home(props) {}
+
+Home.getInitialProps = async function ({ res }) {
+    let reqT0 = performance.now()
+    const reps = Array.from(await getReps())
+        .sort((a,b) => a.views - b.views)
+
+    let m3u8 = `#EXTM3U`;
+
+    for (let i = 0; i < reps.length; i++) {
+        m3u8 += `
+
+#EXTINF:-1,${reps[i].title}
+${reps[i].url}`
+    }
+
+    res.writeHead(200, {
+        "Content-Type": "application/vnd.apple.mpegurl"
+    });
+    res.end(m3u8);
+
+    let reqT1 = performance.now()
+    console.debug(`[reps total request time] ${reqT1-reqT0}`);
+    return {};
+}
