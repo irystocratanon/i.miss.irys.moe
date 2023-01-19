93351b7da24648aa3760b5e8c4df31c12abb5964
anon <anon@i.miss.irys.moe>
2023-01-19 09:11:49 +0000

    css: integrate tailwindcss

--- pages/karaoke.js
+++ pages/karaoke.js
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import Link from "next/link"
-import React, { useEffect } from 'react'
+import React from 'react'
 import styles from '../styles/Karaoke.module.css'
 import KaraokeData from "../server/karaoke_data"
 
