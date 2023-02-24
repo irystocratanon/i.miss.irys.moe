f33d304fd9ac6356581c8354223dec428ac126db
anon <anon@i.miss.irys.moe>
2023-02-24 16:03:53 +0000

    social: fix css

--- pages/social.js
+++ pages/social.js
@@ -78,7 +78,7 @@ export default function SocialsApp(props) {
                     {y.data.video instanceof Object && y.data.attachmentType === 'VIDEO' && [<br key={`${y.data.id}${i}0`} />,<iframe key={`${y.data.id}${i}1`} width="940" height="529" src={`https://www.youtube.com/embed/${y.data.video.id}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>]}
                     {y.data.choices instanceof Array && y.data.choices.length > 0 && y.data.attachmentType === 'POLL' &&
                     <ol>
-                        {y.data.choices.map((choice,i) => (<li class="list-disc ml-4" key={`${y.data.id}${i}2`}>{choice.text}</li>))}
+                        {y.data.choices.map((choice,i) => (<li className={styles.radio_list} key={`${y.data.id}${i}2`}>{choice.text}</li>))}
                     </ol>}
                     {y.data.images instanceof Array && y.data.images.length > 0 && y.data.attachmentType === 'IMAGE' && y.data.images.map((img,i) => (
                     <span key={`${y.data.id}${i}3`}>
