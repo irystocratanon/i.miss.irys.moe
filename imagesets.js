dd3aca9c91bd93a395a6b1c78011be3ba1f9639c
anon <anon@i.miss.irys.moe>
2023-04-27 22:09:17 +0000

    webpify images

--- imagesets.js
+++ imagesets.js
@@ -4,16 +4,16 @@ function importAll(requireModule, pathToImage) {
 
 // The paths have to be written out because invocation of require.context needs to be statically analyzable
 export const ERROR_IMAGE_SET = importAll(
-    require.context('./public/imagesets/errored', false, /\.(gif|png|jpe?g|svg)$/),
+    require.context('./public/imagesets/errored', false, /\.(gif|webp|png|jpe?g|svg)$/),
     'imagesets/errored/'
 );
 
 export const HAVE_STREAM_IMAGE_SET = importAll(
-    require.context('./public/imagesets/have-stream', false, /\.(gif|png|jpe?g|svg)$/),
+    require.context('./public/imagesets/have-stream', false, /\.(gif|webp|png|jpe?g|svg)$/),
     'imagesets/have-stream/'
 );
 
 export const NO_STREAM_IMAGE_SET = importAll(
-    require.context('./public/imagesets/no-stream', false, /\.(gif|png|jpe?g|svg)$/),
+    require.context('./public/imagesets/no-stream', false, /\.(gif|webp|png|jpe?g|svg)$/),
     'imagesets/no-stream/'
 );
