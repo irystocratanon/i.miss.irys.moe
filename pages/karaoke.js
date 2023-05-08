6153efd7ee675dfc5edd34dee690431775f3256a
anon <anon@i.miss.irys.moe>
2023-05-08 14:15:54 +0000

    archives: superchat archives page

--- pages/karaoke.js
+++ pages/karaoke.js
@@ -88,6 +88,8 @@ export default class KaraokeApp extends React.Component {
         <section style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
             <Link href="/">I miss her&hellip;</Link>
             &nbsp;|&nbsp;
+            <Link href="/archives">Archives</Link>
+            &nbsp;|&nbsp;
             <Link href="/milestones">Milestones</Link>
             &nbsp;|&nbsp;
             <Link className="font-bold no-underline" href="/karaoke">Karaoke</Link>
