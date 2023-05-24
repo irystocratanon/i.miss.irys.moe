7c8ac01d29145d61328f672a2d40d54d81204a0e
anon <anon@i.miss.irys.moe>
2023-05-24 07:56:31 +0000

    karaoke: random-redirect exclude member streams if ?member query string is missing

--- pages/karaoke/random.js
+++ pages/karaoke/random.js
@@ -3,9 +3,15 @@ import KaraokeData from "../../server/karaoke_data"
 export default function Home(props) {}
 
 // https://linguinecode.com/post/how-to-redirect-on-the-server-side-with-next-js
-Home.getInitialProps = async function ({ res }) {
+Home.getInitialProps = async function ({ query, res }) {
     let {songs} = KaraokeData;
     songs = songs.filter(e => { return !e.hasOwnProperty('dead') });
+    if (!query.hasOwnProperty('members')) {
+        songs = songs.filter(e => {
+            const title = e.karaoke.title.toLowerCase()
+            return title.indexOf('members only') === -1
+        });
+    }
     let rep
 
     rep = songs[Math.floor(Math.random()*songs.length)]
