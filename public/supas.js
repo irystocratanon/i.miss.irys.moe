ff5fd8f8064698e6eac9eaa39545bc9c4c38e5c6
anon <anon@i.miss.irys.moe>
2023-08-18 01:02:19 +0000

    supas: preserve controls css

--- public/supas.js
+++ public/supas.js
@@ -418,6 +418,12 @@ if (window.performance && performance.getEntriesByType) { // avoid error in Safa
                                     let html = await req.text()
                                     frag.innerHTML=html
                                     document.querySelector("table").replaceWith(frag.content.querySelector("table"))
+                                    const controlCssEl = document.querySelector('#control style')
+                                    if (controlCssEl) {
+                                        let controlCss = document.createElement('style');
+                                        controlCss.innerHTML = controlCssEl.innerHTML;
+                                        document.head.appendChild(controlCss)
+                                    }
                                     document.querySelector("#control").replaceWith(frag.content.querySelector("#control"))
                                     Array.from(frag.content.querySelectorAll("style")).forEach((e,i) => { let targetNode = document.querySelector(".replace-styles"); if (!targetNode) { const css = document.createElement("style"); css.className="replace-styles"; document.body.appendChild(css); targetNode = document.querySelector(".replace-styles") } if (i === 0) { e.className="replace-styles"; targetNode.replaceWith(e) } else { targetNode.parentNode.appendChild(e); }  })
                                 })()
