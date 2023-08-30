aa9e1abe7e10a37d39421697730f7f0861f59f64
anon <anon@i.miss.irys.moe>
2023-08-30 09:35:51 +0000

    cancelled: update

--- server/cancelled.js
+++ server/cancelled.js
@@ -4,6 +4,8 @@ module.exports = {
         "U_IW6CyYn1k", /* 【#ホロお正月CUP2022】ホロライブ、今年も熱くなれ!運も実力も手に入れろ【全体視点有/常闇トワ】(main POV for HOLO NEW YEARS CUP) */
         "Z9ijX3dp4sY", /* holostars POV */
         "sTFkp3YTNIg", /* holostars POV */
-        "uMuXpFZrZDA" /* holostars POV */
+        "uMuXpFZrZDA", /* holostars POV */
+        "1fJ4AJFCRjY", /* holostars POV */
+        "N-LahleJYU0" /* holostars POV */
     ]
 }
