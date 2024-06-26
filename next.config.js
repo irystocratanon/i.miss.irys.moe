4160c022397e9c46aab287e04269ea5440f7ff86
anon <anon@i.miss.irys.moe>
2024-06-26 17:56:59 +0000

    redirect

--- next.config.js
+++ next.config.js
@@ -3,15 +3,30 @@ module.exports = {
     async redirects() {
         return [
             {
+                source: '/',
+                destination: 'https://irys.moe/',
+                permanent: true
+            },
+            {
+                source: '/supas',
+                destination: 'https://irys.moe/supas',
+                permanent: true
+            },
+            {
+                source: '/supas/:slug',
+                destination: 'https://irys.moe/supas/:slug',
+                permanent: true
+            },
+            {
                 source: '/index.html',
-                destination: '/',
+                destination: 'https://irys.moe/',
                 permanent: true
             },
             {
                 source: '/karaokes',
-                destination: '/karaoke',
+                destination: 'https://irys.moe/karaoke',
                 permanent: true
-            }
+            },
         ]
     }
 }
