f5713678df25c584acfe38f66841368f3c02f779
anon <anon@i.miss.irys.moe>
2024-08-27 19:00:20 +0000

    Revert "redirect"
    This reverts commit 4160c022397e9c46aab287e04269ea5440f7ff86.

--- next.config.js
+++ next.config.js
@@ -3,30 +3,15 @@ module.exports = {
     async redirects() {
         return [
             {
-                source: '/',
-                destination: 'https://irys.moe/',
-                permanent: true
-            },
-            {
-                source: '/supas',
-                destination: 'https://irys.moe/supas',
-                permanent: true
-            },
-            {
-                source: '/supas/:slug',
-                destination: 'https://irys.moe/supas/:slug',
-                permanent: true
-            },
-            {
                 source: '/index.html',
-                destination: 'https://irys.moe/',
+                destination: '/',
                 permanent: true
             },
             {
                 source: '/karaokes',
-                destination: 'https://irys.moe/karaoke',
+                destination: '/karaoke',
                 permanent: true
-            },
+            }
         ]
     }
 }
